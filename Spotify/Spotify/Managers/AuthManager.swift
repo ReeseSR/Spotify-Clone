54f7de766b4c41339e6a65a870885e4df94b5cff
bsorinnn <bxxsxrxx@gmail.com>
2021-12-11 23:42:18 +0900

    feat: Auth Set Up

--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "b81d0d7a35064c5aaea30ab3e110afd2"
+        static let clientSecret = "74a56fc3715a4ecfa934763f6946c2e8"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let redirectURI = "https://iosacademy.io/"
+        let scope = "user-read-private"
+        let base = "https://accounts.spotify.com/authorize?"
+        let string = "\(base)response_type=code&client_id=\(Constants.clientID)&scope=\(scope)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping((Bool) -> Void)) {
+        // Get Token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
---
+++ Spotify/Spotify/Resources/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  Spotify
+//
+//  Created by 배소린 on 2021/12/11.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    var width: CGFloat {
+        return frame.size.width
+    }
+    
+    var height: CGFloat {
+        return frame.size.height
+    }
+    
+    var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    var right: CGFloat {
+        return left + width
+    }
+    
+    var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        return top + height
+    }
+}
