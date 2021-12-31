d1c50d865a0dbbec3786523670b66f31f6f5d7f4
bsorinnn <bxxsxrxx@gmail.com>
2021-12-31 23:55:11 +0900

    feat: Part 21- Library UI

---
+++ Spotify/Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by 배소린 on 2021/12/30.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
---
+++ Spotify/Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by 배소린 on 2021/12/30.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
